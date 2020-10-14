3f85e647d0dfe037a69e020008a671ffabe6c9fa
남궁성환 <saturn203@naver.com>
2020-10-14 23:24:03 +0900

    init

---
+++ src/main/java/com/example/vehicleconfigserver/VehicleConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.vehicleconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vehicleconfigserver/VehicleConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vehicleconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
