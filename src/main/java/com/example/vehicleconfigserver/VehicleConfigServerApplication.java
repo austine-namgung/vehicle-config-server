9411cec4c39b2fdc824b9cfb87e1c5d33781b43c
남궁성환 <saturn203@naver.com>
2020-10-22 13:17:06 +0900

    dsfsdn

--- src/main/java/com/example/vehicleconfigserver/VehicleConfigServerApplication.java
+++ src/main/java/com/example/vehicleconfigserver/VehicleConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.example.vehicleconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class VehicleConfigServerApplication {
 
 	public static void main(String[] args) {
